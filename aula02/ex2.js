b9916baf2ff8ef7fe39c8ecea695b8848f70d71f
Kaatykaori <kaatykaori@gmail.com>
2021-10-27 12:33:32 -0300

    atividade PG

---
+++ aula02/ex2.js
@@ -0,0 +1,10 @@
+let nome = "Katy"
+let idade = 28
+let usuarioAtivo = true
+  
+console.log("Nome: ")
+console.log(nome)
+console.log("Idade: ")
+console.log(idade)
+console.log("Usuário está ativo no sistema?")
+console.log(usuarioAtivo)
