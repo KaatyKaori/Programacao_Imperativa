537dc4040a97e50005772a4b08eb211597e4526b
KaatyKaori <kaatykaori@gmail.com>
2021-11-10 12:44:59 -0300

    atividades - mesa e aula

---
+++ aula_08/aula08.js
@@ -0,0 +1,28 @@
+if(true){
+    console.log("entrou no if");
+}else{
+    console.log("entrou no else");
+};
+//////////////////////////////////////
+if(false){
+    console.log("entrou no if");
+}else{
+    console.log("entrou no else");
+};
+
+
+let idade = 20;
+if(idade < 18){
+    console.log("não permitido");
+}else{
+    console.log("permitido");
+};
+
+
+let status = true;
+if(status){
+    console.log("true");
+}else{
+    console.log("false");
+};
+
---
+++ aula_08/aula08mesa.js
@@ -0,0 +1,12 @@
+function podeSubir ( altura, acompanhado){
+    x = altura
+    y = acompanhado 
+    if (x > 1.4 && x < 2){ 
+        return true; }
+        else if (x < 1.40 && y == true){ 
+        return true;} 
+        else (x < 1.20 );{
+            return false;
+        }   
+    }
+console.log 
