8731d914750508c73244f6b47439d7e8b86ed36b
KaatyKaori <kaatykaori@gmail.com>
2021-11-10 12:37:39 -0300

    atividades

---
+++ aula04/ex3.js
@@ -0,0 +1,39 @@
+//EXERCÍCIO ex3 - AULA 04
+
+
+// 1 -  soma dois números
+console.log(7+3)
+
+// 2 - repetir processo com outro operador
+console.log(2*5)
+
+//3 - declare e atribua 2 variáveis
+let number0 = 10;
+let number1 = 20;
+
+console.log(number0+number1);
+
+//  4 - atribuir a soma a uma variável
+let soma = number0+number1;
+console.log(soma);
+
+// 5 - re-atribuir a váriavel a "1" + 1
+soma = "1"+1;
+console.log(soma);
+
+//6 - re-atribuir "olá" + 23
+soma = "olá"+23;
+console.log(soma);
+
+// 7 - mensagem concatenada
+console.log('O valor do number0 é', number0);
+
+// EXERCÍCIO 02 Nutricio
+
+let nome = 'Ana Paula';
+let idade = 26;
+let peso = 55.0
+let altura = 1.62;
+let plano = true;
+let IMC = peso/(altura ^ 2);
+console.log(nome, 'tem', idade, 'anos. O seu IMC é de', IMC);
