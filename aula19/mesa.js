c75345bfb949f8f91212a82e834bff09830c6b67
KaatyKaori <kaatykaori@gmail.com>
2021-12-07 18:49:16 -0300

    atividade aula - mesa

--- aula19/mesa.js
+++ aula19/mesa.js
@@ -6,31 +6,31 @@ console.log(pessoas);
 
 
 // a maior e a menor altura do grupo;
-function MaioreMenorAltura(array){
+function TallerAndShorter(array){
     pessoas.sort((a,b) => {return b.altura - a.altura});
     console.log('A pessoa com maior altura é : ' + pessoas[0].nome + ' com ' + pessoas[0].altura + 'm.');
     pessoas.sort((a,b) => {return a.altura - b.altura});
     console.log('A pessoa com menor altura é : ' + pessoas[0].nome + ' com ' + pessoas[0].altura + 'm.');
 }
 
-MaioreMenorAltura(pessoas);
+TallerAndShorter(pessoas);
 
 // a média de altura das mulheres;
 
 
-function MediaAltura(array) {
+function MediumHeight(array) {
     let mulher = array.filter((pessoa) => {return pessoa.sexo == 'F'});
     let soma = mulher.reduce((acumulador,valor) => acumulador + valor.altura, 0);
     let media = soma / mulher.length;
     return 'A média de altura das mulheres é: ' + media;
 }
 
-console.log(MediaAltura(pessoas));
+console.log(MediumHeight(pessoas));
 
 // o número de homens:
-function NumeroHomens(array) {
+function MenNumber(array) {
     let homem = array.filter((pessoa) => {return pessoa.sexo == 'M'});
     return 'A quantidade de homens é: ' + homem.length;
 }
 
-console.log(NumeroHomens(pessoas));
+console.log(MenNumber(pessoas));
